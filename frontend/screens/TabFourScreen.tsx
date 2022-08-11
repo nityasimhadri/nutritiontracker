0922a6185dd4808a1bde812a102d373c849b5008
sidsimhadri <sidsimhadri@gmail.com>
2022-08-11 00:11:56 -0700

    buttons on activity

--- frontend/screens/TabFourScreen.tsx
+++ frontend/screens/TabFourScreen.tsx
@@ -1,16 +1,93 @@
 import { StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
-import { Center, Container, VStack } from 'native-base';
-import { Text, View } from '../components/Themed';
+import { Text, Box, Pressable, HStack, VStack } from 'native-base';
+import { View } from '../components/Themed';
 
 
 export default function TabFourScreen() {
   return (
     <View style={styles.container}>
+    <VStack space={4} alignItems="center" width="90%" >
     <VStack space={4} alignItems="center">
-      <Center w="64" h="20" bg="indigo.300" rounded="md" shadow={3} />
-      <Center w="64" h="20" bg="indigo.500" rounded="md" shadow={3} />
-      <Center w="64" h="20" bg="indigo.700" rounded="md" shadow={3} />
+    <Box alignItems="center">
+      <Pressable>
+        {({
+        isHovered,
+        isPressed
+      }) => {
+        return <Box width='90%' borderWidth="1" borderColor="coolGray.300" shadow="3" bg={isPressed ? "coolGray.200" : isHovered ? "coolGray.200" : "coolGray.100"} p="5" rounded="8" style={{
+          transform: [{
+            scale: isPressed ? 0.96 : 1
+          }]
+        }}>
+          <HStack space={4}>
+              <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
+                Quick Start
+              </Text>
+              <FontAwesome name="play" size={24} color="black" />
+              </HStack>
+              <Text mt="2" fontSize="sm" color="coolGray.700">
+                Start an empty workout
+              </Text>
+
+            </Box>;
+      }}
+      </Pressable>
+    </Box>
+    <HStack width="90%" space={4} alignItems="center">
+      <Box alignItems="center">
+      <Pressable>
+        {({
+        isHovered,
+        isPressed
+      }) => {
+        return <Box borderWidth="1" borderColor="coolGray.300" shadow="3" bg={isPressed ? "coolGray.200" : isHovered ? "coolGray.200" : "coolGray.100"} p="5" rounded="8" style={{
+          transform: [{
+            scale: isPressed ? 0.96 : 1
+          }]
+        }}>
+            <HStack space={4}>
+              <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
+                New Routine
+              </Text>
+              <FontAwesome name="plus" size={24} color="black" />
+              </HStack>
+              <Text mt="2" fontSize="sm" color="coolGray.700" >
+                Add a new fitness plan
+              </Text>
+
+            </Box>;
+      }}
+      </Pressable>
+    </Box>
+    <Box alignItems="center">
+      <Pressable>
+        {({
+        isHovered,
+        isPressed
+      }) => {
+        return <Box borderWidth="1" borderColor="coolGray.300" shadow="3" bg={isPressed ? "coolGray.200" : isHovered ? "coolGray.200" : "coolGray.100"} p="5" rounded="8" style={{
+          transform: [{
+            scale: isPressed ? 0.96 : 1
+          }]
+        }}>
+            <HStack space={4}>
+              <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
+                Search Routines
+              </Text>
+              <FontAwesome name="search" size={24} color="black" />
+              </HStack>
+              <Text mt="2" fontSize="sm" color="coolGray.700">
+                Search saved routines
+              </Text>
+
+            </Box>;
+      }}
+      </Pressable>
+    </Box>
+      </HStack>
+    </VStack>
     </VStack>
     </View>
   );
@@ -27,6 +104,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
+  
     
   },
   title: {
